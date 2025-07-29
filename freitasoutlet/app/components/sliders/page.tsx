be992d23aa6f990267dbae1701f95d3f5b6be1c1
Arthur Fellipe <arthur.fellipe.brandao@gmail.com>
2025-07-29 15:42:03 -0300

    update

--- freitasoutlet/app/components/sliders/page.tsx
+++ freitasoutlet/app/components/sliders/page.tsx
@@ -1,13 +1,48 @@
+'use client'
+
+import { useState } from 'react'
 import FreitasOutlet1 from '@/public/Arte 1.png'
 import FreitasOutlet2 from '@/public/Arte 2.png'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+
+const images = [FreitasOutlet1, FreitasOutlet2]
 
 export default function Sliders() {
-    return(
-        <div>
+  const [currentIndex, setCurrentIndex] = useState(0)
 
-        <img src={FreitasOutlet1.src} alt="" />
-        <img src={FreitasOutlet2.src} alt="" />
-
-        </div>
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
     )
+  }
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    )
+  }
+
+  return (
+    <div className="relative mt-[2rem] w-full mx-auto">
+      <img
+        src={images[currentIndex].src}
+        alt={`Slide ${currentIndex + 1}`}
+        className="w-full rounded-lg shadow-md animate-fade-right animate-duration-[3000ms]"
+      />
+
+      <button
+        onClick={prevSlide}
+        className="absolute top-1/2 left-4 text-[2rem] bg-gray-500 w-[2rem] h-[2rem] flex items-center justify-center rounded cursor-pointer font-bold hover:bg-black hover:text-white"
+      >
+        <ChevronLeft />
+      </button>
+
+      <button
+        onClick={nextSlide}
+        className="absolute top-1/2 right-4 w-[2rem] h-[2rem] flex items-center justify-center bg-gray-500 rounded cursor-pointer font-bold hover:bg-black hover:text-white"
+      >
+        <ChevronRight />
+      </button>
+    </div>
+  )
 }
--- freitasoutlet/app/page.tsx
+++ freitasoutlet/app/page.tsx
@@ -1,12 +1,14 @@
 import Contain from './components/contain/page';
 import Header from './components/header/header';
 import Descont from './components/descont/page';
+import Sliders from './components/sliders/page';
 
 export default function Home() {
   return (
     <>
     <Descont />
     <Header />
+    <Sliders />
     </>
   );
 }
